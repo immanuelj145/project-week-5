9413728a8e53266eb757b0f28dac9418475ccb52
immanuelj145 <immanuelj@merrimack.edu>
2026-06-17 20:02:06 -0400

    uploading javadocs

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"FlexibleNotificationSystem","l":"FlexibleNotificationSystem()","u":"%3Cinit%3E()","k":"3"},{"p":"<Unnamed>","c":"FlexibleNotificationSystem","l":"main(String[])","u":"main(java.lang.String[])","k":"6"}];updateSearchResults();
